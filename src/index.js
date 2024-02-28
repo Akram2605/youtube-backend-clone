3badf548a9de8eae05d755cfd5fc7b92cdf12ac7
Akram2605 <akrammemon2605@gmail.com>
2024-02-28 12:38:26 +0530

    MongoDB connection

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import 'dotenv/config'
+import connectDB from "./db/index.js";
+
+connectDB()
+
+
+
+
+
+
+
+
+
+// require ('dotenv').config({path:'./env'})
+
+// import dotenv, { config } from "dotenv"
+
+
+// dotenv.config({
+//    path:'./env'
+// })
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import { express } from "express";
+//  const app=express()
+//iffi------>
+// (async()=> {
+//     try{
+//          await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//          app.on("error",(error)=>{
+//             console.log("ERROR:",error);
+//             throw error
+//          })
+
+//          app.listen(process.env.PORT,()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//          })
+//        }
+//     catch(error){
+//         console.error("ERROR:",error)
+//         throw err
+//                 }
+// })()
